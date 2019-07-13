1a3387b15507e59a314888ece6899e438a6fc962
Jamie Sherman <jamie.sherman@gmail.com>
2019-07-12 22:58:16 -0700

    improved formatting

--- _pylibczi/_pylibczi.cpp
+++ _pylibczi/_pylibczi.cpp
@@ -17,7 +17,9 @@
 // cpp wrapper for libCZI for reading Zeiss czi file scene data.
 
 #include "Python.h"
+
 #define NPY_NO_DEPRECATED_API NPY_1_14_API_VERSION
+
 #include "numpy/arrayobject.h"
 
 #include <cstdio>
@@ -31,9 +33,9 @@
 #include "aics_added.hpp"
 
 // https://stackoverflow.com/questions/3342726/c-print-out-enum-value-as-text
-std::ostream& operator<<(std::ostream& out, const libCZI::PixelType value){
-    static std::map<libCZI::PixelType, std::string> strings;
-    if (strings.size() == 0){
+std::ostream &operator<<(std::ostream &out, const libCZI::PixelType value) {
+    static std::map <libCZI::PixelType, std::string> strings;
+    if (strings.size() == 0) {
 #define INSERT_ELEMENT(p) strings[p] = #p
         INSERT_ELEMENT(libCZI::PixelType::Invalid);
         INSERT_ELEMENT(libCZI::PixelType::Gray8);
@@ -53,9 +55,9 @@ std::ostream& operator<<(std::ostream& out, const libCZI::PixelType value){
     return out << strings[value];
 }
 
-std::ostream& operator<<(std::ostream& out, const libCZI::DimensionIndex value){
-    static std::map<libCZI::DimensionIndex, std::string> strings;
-    if (strings.size() == 0){
+std::ostream &operator<<(std::ostream &out, const libCZI::DimensionIndex value) {
+    static std::map <libCZI::DimensionIndex, std::string> strings;
+    if (strings.size() == 0) {
 #define INSERT_ELEMENT(p) strings[p] = #p
         INSERT_ELEMENT(libCZI::DimensionIndex::invalid);
         INSERT_ELEMENT(libCZI::DimensionIndex::MinDim);
@@ -80,13 +82,16 @@ std::ostream& operator<<(std::ostream& out, const libCZI::DimensionIndex value){
 // .... Python callable extensions ..................
 
 static PyObject *cziread_meta(PyObject *self, PyObject *args);
+
 static PyObject *cziread_scene(PyObject *self, PyObject *args);
+
 static PyObject *cziread_allsubblocks(PyObject *self, PyObject *args);
 static PyObject *cziread_mosaic_shape(PyObject *self, PyObject *args);
 static PyObject *cziread_mosaic(PyObject *self, PyObject *args);
 
 /* ==== Set up the methods table ====================== */
 static PyMethodDef _pylibcziMethods[] = {
+<<<<<<< HEAD
     {"cziread_meta", cziread_meta, METH_VARARGS, "Read czi meta data"},
     {"cziread_scene", cziread_scene, METH_VARARGS, "Read czi scene image"},
     {"cziread_allsubblocks", cziread_allsubblocks, METH_VARARGS, "Read czi image containing all scenes"},
@@ -119,8 +124,7 @@ static struct PyModuleDef moduledef = {
 static PyObject *PylibcziError;
 
 extern "C" {
-PyMODINIT_FUNC PyInit__pylibczi(void)
-{
+PyMODINIT_FUNC PyInit__pylibczi(void) {
     PyObject *module = PyModule_Create(&moduledef);
 
     if (module == NULL)
@@ -138,8 +142,9 @@ PyMODINIT_FUNC PyInit__pylibczi(void)
 
 /* #### Helper prototypes ################################### */
 
-std::shared_ptr<libCZI::ICZIReader> open_czireader_from_cfilename(char const *fn);
-PyArrayObject* copy_bitmap_to_numpy_array(std::shared_ptr<libCZI::IBitmapData> pBitmap);
+std::shared_ptr <libCZI::ICZIReader> open_czireader_from_cfilename(char const *fn);
+
+PyArrayObject *copy_bitmap_to_numpy_array(std::shared_ptr <libCZI::IBitmapData> pBitmap);
 
 /* #### Extended modules #################################### */
 
@@ -158,7 +163,7 @@ static PyObject *cziread_meta(PyObject *self, PyObject *args) {
     //auto dsplSettings = docInfo->GetDisplaySettings();
     std::string xml = md->GetXml();
     // copy the metadata into python string
-    PyObject* pystring = Py_BuildValue("s", xml.c_str());
+    PyObject *pystring = Py_BuildValue("s", xml.c_str());
 
     cziReader->Close();
     return pystring;
@@ -191,11 +196,10 @@ static PyObject *cziread_allsubblocks(PyObject *self, PyObject *args) {
     npy_intp subblock_count = 0;
 
     cziReader->EnumerateSubBlocks(
-        [&subblock_count](int idx, const libCZI::SubBlockInfo& info)
-    {
-        subblock_count++;
-        return true;
-    });
+            [&subblock_count](int idx, const libCZI::SubBlockInfo &info) {
+                subblock_count++;
+                return true;
+            });
     //std::cout << "Enumerated " << subblock_count << std::endl;
 
     // meh - this seems to be not useful, what is an M-index? someone read the spec...
@@ -204,34 +208,36 @@ static PyObject *cziread_allsubblocks(PyObject *self, PyObject *args) {
     //int subblock_count = stats.subBlockCount;
 
     // copy the image data and coordinates into numpy arrays, return images as python list of numpy arrays
-    PyObject* images = PyList_New(subblock_count);
-    npy_intp eshp[2]; eshp[0] = subblock_count; eshp[1] = 2;
+    PyObject *images = PyList_New(subblock_count);
+    npy_intp eshp[2];
+    eshp[0] = subblock_count;
+    eshp[1] = 2;
     PyArrayObject *coordinates = (PyArrayObject *) PyArray_Empty(2, eshp, PyArray_DescrFromType(NPY_INT32), 0);
     npy_int32 *coords = (npy_int32 *) PyArray_DATA(coordinates);
 
     npy_intp cnt = 0;
     cziReader->EnumerateSubBlocks(
-        [&cziReader, &cnt, images, coords](int idx, const libCZI::SubBlockInfo& info)
-    {
-        //std::cout << "Index " << idx << ": " << libCZI::Utils::DimCoordinateToString(&info.coordinate)
-        //  << " Rect=" << info.logicalRect << " M-index " << info.mIndex << std::endl;
+            [&cziReader, &cnt, images, coords](int idx, const libCZI::SubBlockInfo &info) {
+                //std::cout << "Index " << idx << ": " << libCZI::Utils::DimCoordinateToString(&info.coordinate)
+                //  << " Rect=" << info.logicalRect << " M-index " << info.mIndex << std::endl;
 
-        // add the sub-block image
-        PyList_SetItem(images, cnt,
-            (PyObject*) copy_bitmap_to_numpy_array(cziReader->ReadSubBlock(idx)->CreateBitmap()));
-        // add the coordinates
-        coords[2*cnt] = info.logicalRect.x; coords[2*cnt+1] = info.logicalRect.y;
+                // add the sub-block image
+                PyList_SetItem(images, cnt,
+                               (PyObject *) copy_bitmap_to_numpy_array(cziReader->ReadSubBlock(idx)->CreateBitmap()));
+                // add the coordinates
+                coords[2 * cnt] = info.logicalRect.x;
+                coords[2 * cnt + 1] = info.logicalRect.y;
 
-        //info.coordinate.EnumValidDimensions([](libCZI::DimensionIndex dim, int value)
-        //{
-        //    //valid_dims[(int) dim] = true;
-        //    cout << "Dimension  " << dim << " value " << value << endl;
-        //    return true;
-        //});
+                //info.coordinate.EnumValidDimensions([](libCZI::DimensionIndex dim, int value)
+                //{
+                //    //valid_dims[(int) dim] = true;
+                //    cout << "Dimension  " << dim << " value " << value << endl;
+                //    return true;
+                //});
 
-        cnt++;
-        return true;
-    });
+                cnt++;
+                return true;
+            });
 
     return Py_BuildValue("OO", images, (PyObject *) coordinates);
 }
@@ -387,18 +393,20 @@ static PyObject *cziread_scene(PyObject *self, PyObject *args) {
 
     // get either the scene or a bounding box on the scene to load
     npy_intp size_scene_or_box = PyArray_SIZE(scene_or_box);
-    if( PyArray_TYPE(scene_or_box) != NPY_INT64 ) {
+    if (PyArray_TYPE(scene_or_box) != NPY_INT64) {
         PyErr_SetString(PylibcziError, "Scene or box argument must be int64");
         return NULL;
     }
-    npy_int64 *ptr_scene_or_box = (npy_int64*) PyArray_DATA(scene_or_box);
-    bool use_scene; npy_int32 scene = -1; npy_int32 rect[4];
-    if( size_scene_or_box == 1 ) {
+    npy_int64 *ptr_scene_or_box = (npy_int64 *) PyArray_DATA(scene_or_box);
+    bool use_scene;
+    npy_int32 scene = -1;
+    npy_int32 rect[4];
+    if (size_scene_or_box == 1) {
         use_scene = true;
         scene = ptr_scene_or_box[0];
-    } else if( size_scene_or_box == 4 ) {
+    } else if (size_scene_or_box == 4) {
         use_scene = false;
-        for( int i=0; i < 4; i++ ) rect[i] = ptr_scene_or_box[i];
+        for (int i = 0; i < 4; i++) rect[i] = ptr_scene_or_box[i];
     } else {
         PyErr_SetString(PylibcziError, "Second input must be size 1 (scene) or 4 (box)");
         return NULL;
@@ -409,40 +417,47 @@ static PyObject *cziread_scene(PyObject *self, PyObject *args) {
     // if only the scene was given the enumerate subblocks to get limits, otherwise use the provided bounding box.
     int min_x, min_y, max_x, max_y, size_x, size_y;
     //std::vector<bool> valid_dims ((int) libCZI::DimensionIndex::MaxDim, false);
-    if( use_scene ) {
+    if (use_scene) {
         // enumerate subblocks, get the min and max coordinates of the specified scene
-        min_x = std::numeric_limits<int>::max(); min_y = std::numeric_limits<int>::max(); max_x = -1; max_y = -1;
+        min_x = std::numeric_limits<int>::max();
+        min_y = std::numeric_limits<int>::max();
+        max_x = -1;
+        max_y = -1;
         cziReader->EnumerateSubBlocks(
-            //[scene, &min_x, &min_y, &max_x, &max_y, &valid_dims](int idx, const libCZI::SubBlockInfo& info)
-            [scene, &min_x, &min_y, &max_x, &max_y](int idx, const libCZI::SubBlockInfo& info)
-        {
-            int cscene = 0;
-            info.coordinate.TryGetPosition(libCZI::DimensionIndex::S, &cscene);
-            // negative value for scene indicates to load all scenes
-            if( cscene == scene || scene < 0 ) {
-                //cout << "Index " << idx << ": " << libCZI::Utils::DimCoordinateToString(&info.coordinate)
-                //  << " Rect=" << info.logicalRect << " scene " << scene << endl;
-                auto rect = info.logicalRect;
-                if( rect.x < min_x ) min_x = rect.x;
-                if( rect.y < min_y ) min_y = rect.y;
-                if( rect.x + rect.w > max_x ) max_x = rect.x + rect.w;
-                if( rect.y + rect.h > max_y ) max_y = rect.y + rect.h;
-            }
+                //[scene, &min_x, &min_y, &max_x, &max_y, &valid_dims](int idx, const libCZI::SubBlockInfo& info)
+                [scene, &min_x, &min_y, &max_x, &max_y](int idx, const libCZI::SubBlockInfo &info) {
+                    int cscene = 0;
+                    info.coordinate.TryGetPosition(libCZI::DimensionIndex::S, &cscene);
+                    // negative value for scene indicates to load all scenes
+                    if (cscene == scene || scene < 0) {
+                        //cout << "Index " << idx << ": " << libCZI::Utils::DimCoordinateToString(&info.coordinate)
+                        //  << " Rect=" << info.logicalRect << " scene " << scene << endl;
+                        auto rect = info.logicalRect;
+                        if (rect.x < min_x) min_x = rect.x;
+                        if (rect.y < min_y) min_y = rect.y;
+                        if (rect.x + rect.w > max_x) max_x = rect.x + rect.w;
+                        if (rect.y + rect.h > max_y) max_y = rect.y + rect.h;
+                    }
 
-            //info.coordinate.EnumValidDimensions(
-            //    [&valid_dims](libCZI::DimensionIndex dim, int value)
-            //{
-            //    valid_dims[(int) dim] = true;
-            //    //cout << "Dimension  " << dim << " value " << value << endl;
-            //    return true;
-            //});
+                    //info.coordinate.EnumValidDimensions(
+                    //    [&valid_dims](libCZI::DimensionIndex dim, int value)
+                    //{
+                    //    valid_dims[(int) dim] = true;
+                    //    //cout << "Dimension  " << dim << " value " << value << endl;
+                    //    return true;
+                    //});
 
-            return true;
-        });
-        size_x = max_x-min_x; size_y = max_y-min_y;
+                    return true;
+                });
+        size_x = max_x - min_x;
+        size_y = max_y - min_y;
     } else {
-        min_x = rect[0]; size_x = rect[2]; min_y = rect[1]; size_y = rect[3];
-        max_x = min_x + size_x; max_y = min_y + size_y;
+        min_x = rect[0];
+        size_x = rect[2];
+        min_y = rect[1];
+        size_y = rect[3];
+        max_x = min_x + size_x;
+        max_y = min_y + size_y;
     }
     //std::cout << "min x y " << min_x << " " << min_y << " max x y " << max_x << " " << max_y << std::endl;
     //for (auto it = valid_dims.begin(); it != valid_dims.end(); ++it) {
@@ -459,30 +474,38 @@ static PyObject *cziread_scene(PyObject *self, PyObject *args) {
     //   commented code above creates bool vector saying which dims are valid (in any subblock).
     //   it is possible for a czi file to not have any valid dims, not sure what this means exactly.
     //libCZI::CDimCoordinate planeCoord{ { libCZI::DimensionIndex::Z,0 } };
-    libCZI::CDimCoordinate planeCoord{ { libCZI::DimensionIndex::C,0 } };
+    libCZI::CDimCoordinate planeCoord{{libCZI::DimensionIndex::C, 0}};
     auto multiTileComposit = accessor->Get(
-        libCZI::IntRect{ min_x, min_y, size_x, size_y },
-        &planeCoord,
-        nullptr);   // use default options
+            libCZI::IntRect{min_x, min_y, size_x, size_y},
+            &planeCoord,
+            nullptr);   // use default options
 
-    PyArrayObject* img = copy_bitmap_to_numpy_array(multiTileComposit);
+    PyArrayObject *img = copy_bitmap_to_numpy_array(multiTileComposit);
 
     cziReader->Close();
-    return (PyObject*) img;
+    return (PyObject *) img;
 }
 
-PyArrayObject* copy_bitmap_to_numpy_array(std::shared_ptr<libCZI::IBitmapData> pBitmap) {
+PyArrayObject *copy_bitmap_to_numpy_array(std::shared_ptr <libCZI::IBitmapData> pBitmap) {
     // define numpy types/shapes and bytes per pixel depending on the zeiss bitmap pixel type.
-    int numpy_type = NPY_UINT16; int pixel_size_bytes = 0; int channels = 1;
-    switch( pBitmap->GetPixelType() ) {
+    int numpy_type = NPY_UINT16;
+    int pixel_size_bytes = 0;
+    int channels = 1;
+    switch (pBitmap->GetPixelType()) {
         case libCZI::PixelType::Gray8:
-            numpy_type = NPY_UINT8; pixel_size_bytes = 1; channels = 1;
+            numpy_type = NPY_UINT8;
+            pixel_size_bytes = 1;
+            channels = 1;
             break;
         case libCZI::PixelType::Gray16:
-            numpy_type = NPY_UINT16; pixel_size_bytes = 2; channels = 1;
+            numpy_type = NPY_UINT16;
+            pixel_size_bytes = 2;
+            channels = 1;
             break;
         case libCZI::PixelType::Bgr48:
-            numpy_type = NPY_UINT16; pixel_size_bytes = 6; channels = 3;
+            numpy_type = NPY_UINT16;
+            pixel_size_bytes = 6;
+            channels = 3;
             break;
         default:
             std::cout << pBitmap->GetPixelType() << std::endl;
@@ -495,19 +518,27 @@ PyArrayObject* copy_bitmap_to_numpy_array(std::shared_ptr<libCZI::IBitmapData> p
     int size_x = size.w, size_y = size.h;
     PyArrayObject *img;
     int swap_axes[2];
-    if( channels==1 ) {
-        npy_intp shp[2]; shp[0] = size_x; shp[1] = size_y;
+    if (channels == 1) {
+        npy_intp shp[2];
+        shp[0] = size_x;
+        shp[1] = size_y;
         // images in czi file are in F-order, set F-order flag (last argument to PyArray_Empty)
         img = (PyArrayObject *) PyArray_Empty(2, shp, PyArray_DescrFromType(numpy_type), 1);
-        swap_axes[0] = 0; swap_axes[1] = 1;
+        swap_axes[0] = 0;
+        swap_axes[1] = 1;
     } else {
-        npy_intp shp[3]; shp[1] = size_x; shp[2] = size_y; shp[0] = channels;
+        npy_intp shp[3];
+        shp[1] = size_x;
+        shp[2] = size_y;
+        shp[0] = channels;
         // images in czi file are in F-order, set F-order flag (last argument to PyArray_Empty)
         img = (PyArrayObject *) PyArray_Empty(3, shp, PyArray_DescrFromType(numpy_type), 1);
-        swap_axes[0] = 0; swap_axes[1] = 2;
+        swap_axes[0] = 0;
+        swap_axes[1] = 2;
     }
     void *pointer = PyArray_DATA(img);
 
+    // copy from the czi lib image pointer to the numpy array pointer
     std::size_t rowsize =  size_x * pixel_size_bytes;
     {
         libCZI::ScopedBitmapLockerP lckScoped{pBitmap.get()};
@@ -518,10 +549,10 @@ PyArrayObject* copy_bitmap_to_numpy_array(std::shared_ptr<libCZI::IBitmapData> p
         }
     }
     // transpose to convert from F-order to C-order array
-    return (PyArrayObject*) PyArray_SwapAxes(img,swap_axes[0],swap_axes[1]);
+    return (PyArrayObject *) PyArray_SwapAxes(img, swap_axes[0], swap_axes[1]);
 }
 
-std::shared_ptr<libCZI::ICZIReader> open_czireader_from_cfilename(char const *fn) {
+std::shared_ptr <libCZI::ICZIReader> open_czireader_from_cfilename(char const *fn) {
     // open the czi file
     // https://msdn.microsoft.com/en-us/library/ms235631.aspx
     size_t newsize = strlen(fn) + 1;
@@ -530,7 +561,7 @@ std::shared_ptr<libCZI::ICZIReader> open_czireader_from_cfilename(char const *fn
     // in the new format. If you want to add more characters
     // to the end of the string, increase the value of newsize
     // to increase the size of the buffer.
-    wchar_t * wcstring = new wchar_t[newsize];
+    wchar_t *wcstring = new wchar_t[newsize];
     // Convert char* string to a wchar_t* string.
     //size_t convertedChars = mbstowcs(wcstring, fn, newsize);
     mbstowcs(wcstring, fn, newsize);
--- _pylibczi/aics_added.hpp
+++ _pylibczi/aics_added.hpp
@@ -17,50 +17,48 @@ using namespace std;
 
 class CSimpleStreamImplFromFP;
 
-std::shared_ptr<CSimpleStreamImplFromFP> cziread_io_buffered_reader_to_istream(PyObject *self, PyObject *args);
-std::shared_ptr<libCZI::ICZIReader> open_czireader_from_istream(std::shared_ptr<CSimpleStreamImplFromFP>stream);
-PyArrayObject* copy_bitmap_to_numpy_array(std::shared_ptr<libCZI::IBitmapData> pBitmap);
+std::shared_ptr <CSimpleStreamImplFromFP> cziread_io_buffered_reader_to_istream(PyObject *self, PyObject *args);
+
+std::shared_ptr <libCZI::ICZIReader> open_czireader_from_istream(std::shared_ptr <CSimpleStreamImplFromFP> stream);
+
+PyArrayObject *copy_bitmap_to_numpy_array(std::shared_ptr <libCZI::IBitmapData> pBitmap);
 
 /// <summary>	A wrapper that takes a FILE * and creates an IStream object out of it
-class CSimpleStreamImplFromFP : public libCZI::IStream
-{
+class CSimpleStreamImplFromFP : public libCZI::IStream {
 private:
-    FILE* fp;
+    FILE *fp;
 public:
     CSimpleStreamImplFromFP() = delete;
 
-    explicit CSimpleStreamImplFromFP(FILE* file_pointer): fp(file_pointer) {}
+    explicit CSimpleStreamImplFromFP(FILE *file_pointer) : fp(file_pointer) {}
 
     ~CSimpleStreamImplFromFP() override = default;
-public:	// interface libCZI::IStream
-    void Read(std::uint64_t offset, void *pv, std::uint64_t size, std::uint64_t* ptrBytesRead) override {
+
+public:    // interface libCZI::IStream
+    void Read(std::uint64_t offset, void *pv, std::uint64_t size, std::uint64_t *ptrBytesRead) override {
         int r = fseeko(this->fp, offset, SEEK_SET);
 
-        std::uint64_t bytesRead = fread(pv, 1, (size_t)size, this->fp);
-        if (ptrBytesRead != nullptr)
-        {
+        std::uint64_t bytesRead = fread(pv, 1, (size_t) size, this->fp);
+        if (ptrBytesRead != nullptr) {
             *ptrBytesRead = bytesRead;
         }
     }
 };
 
 class BadFileDescriptorException : public std::exception {
-    const char * what () const throw ()
-    {
-    	return "Couldn't Convert Python File stream to FILE *";
+    const char *what() const throw() {
+        return "Couldn't Convert Python File stream to FILE *";
     }
 };
 
 class BadArgsException : public std::exception {
-    const char * what () const throw ()
-    {
-    	return "Couldn't Convert Python Arguments to C++ objects";
+    const char *what() const throw() {
+        return "Couldn't Convert Python Arguments to C++ objects";
     }
 };
 
 
-static PyObject *cziread_meta_from_istream(PyObject *self, PyObject *args)
-{
+static PyObject *cziread_meta_from_istream(PyObject *self, PyObject *args) {
     auto stream = cziread_io_buffered_reader_to_istream(self, args);
     auto cziReader = open_czireader_from_istream(stream);
 
@@ -71,23 +69,21 @@ static PyObject *cziread_meta_from_istream(PyObject *self, PyObject *args)
     //auto dsplSettings = docInfo->GetDisplaySettings();
     std::string xml = md->GetXml();
     // copy the metadata into python string
-    PyObject* pystring = Py_BuildValue("s", xml.c_str());
+    PyObject *pystring = Py_BuildValue("s", xml.c_str());
 
     cziReader->Close();
     return pystring;
 }
 
-static PyObject *cziread_allsubblocks_from_istream(PyObject *self, PyObject *args)
-{
+static PyObject *cziread_allsubblocks_from_istream(PyObject *self, PyObject *args) {
     // parse arguments
-    try{
+    try {
         auto stream = cziread_io_buffered_reader_to_istream(self, args);
         auto cziReader = open_czireader_from_istream(stream);
-           // count all the subblocks
+        // count all the subblocks
         npy_intp subblock_count = 0;
         cziReader->EnumerateSubBlocks(
-                [&subblock_count](int idx, const libCZI::SubBlockInfo& info)
-                {
+                [&subblock_count](int idx, const libCZI::SubBlockInfo &info) {
                     subblock_count++;
                     return true;
                 });
@@ -99,23 +95,26 @@ static PyObject *cziread_allsubblocks_from_istream(PyObject *self, PyObject *arg
         //int subblock_count = stats.subBlockCount;
 
         // copy the image data and coordinates into numpy arrays, return images as python list of numpy arrays
-        PyObject* images = PyList_New(subblock_count);
-        npy_intp eshp[2]; eshp[0] = subblock_count; eshp[1] = 2;
+        PyObject *images = PyList_New(subblock_count);
+        npy_intp eshp[2];
+        eshp[0] = subblock_count;
+        eshp[1] = 2;
         PyArrayObject *coordinates = (PyArrayObject *) PyArray_Empty(2, eshp, PyArray_DescrFromType(NPY_INT32), 0);
         npy_int32 *coords = (npy_int32 *) PyArray_DATA(coordinates);
 
         npy_intp cnt = 0;
         cziReader->EnumerateSubBlocks(
-                [&cziReader, &subblock_count, &cnt, images, coords](int idx, const libCZI::SubBlockInfo& info)
-                {
+                [&cziReader, &subblock_count, &cnt, images, coords](int idx, const libCZI::SubBlockInfo &info) {
                     //std::cout << "Index " << idx << ": " << libCZI::Utils::DimCoordinateToString(&info.coordinate)
                     //  << " Rect=" << info.logicalRect << " M-index " << info.mIndex << std::endl;
 
                     // add the sub-block image
                     PyList_SetItem(images, cnt,
-                                   (PyObject*) copy_bitmap_to_numpy_array(cziReader->ReadSubBlock(idx)->CreateBitmap()));
+                                   (PyObject *) copy_bitmap_to_numpy_array(
+                                           cziReader->ReadSubBlock(idx)->CreateBitmap()));
                     // add the coordinates
-                    coords[2*cnt] = info.logicalRect.x; coords[2*cnt+1] = info.logicalRect.y;
+                    coords[2 * cnt] = info.logicalRect.x;
+                    coords[2 * cnt + 1] = info.logicalRect.y;
 
                     //info.coordinate.EnumValidDimensions([](libCZI::DimensionIndex dim, int value)
                     //{
@@ -128,31 +127,31 @@ static PyObject *cziread_allsubblocks_from_istream(PyObject *self, PyObject *arg
                     return true;
                 });
 
-    return Py_BuildValue("OO", images, (PyObject *) coordinates);
+        return Py_BuildValue("OO", images, (PyObject *) coordinates);
     }
-    catch (const BadArgsException &e){
+    catch (const BadArgsException &e) {
         PyErr_SetString(PyExc_TypeError, "Unable to map args provided from python to c++.");
         return NULL;
     }
-    catch (const BadFileDescriptorException &fbad){
+    catch (const BadFileDescriptorException &fbad) {
         PyErr_SetString(PyExc_IOError, "Unable to convert ByteIO object to File pointer.");
         return NULL;
     }
     return NULL;
 }
 
-std::shared_ptr<CSimpleStreamImplFromFP> cziread_io_buffered_reader_to_istream(PyObject *self, PyObject *args) {
+std::shared_ptr <CSimpleStreamImplFromFP> cziread_io_buffered_reader_to_istream(PyObject *self, PyObject *args) {
     // parse arguments
     PyObject *in_file;
     if (!PyArg_ParseTuple(args, "O", &in_file)) throw BadArgsException();
     int fdescript = PyObject_AsFileDescriptor(in_file);
-    if( fdescript == -1 ) throw BadFileDescriptorException();
-    FILE *fp = fdopen(fdescript,"r");
-    if( fp == nullptr ) throw BadFileDescriptorException();
+    if (fdescript == -1) throw BadFileDescriptorException();
+    FILE *fp = fdopen(fdescript, "r");
+    if (fp == nullptr) throw BadFileDescriptorException();
     return std::make_shared<CSimpleStreamImplFromFP>(fp);
 }
 
-std::shared_ptr<libCZI::ICZIReader> open_czireader_from_istream(std::shared_ptr<CSimpleStreamImplFromFP> stream) {
+std::shared_ptr <libCZI::ICZIReader> open_czireader_from_istream(std::shared_ptr <CSimpleStreamImplFromFP> stream) {
     // open the czi file
     auto cziReader = libCZI::CreateCZIReader();
     cziReader->Open(stream);
