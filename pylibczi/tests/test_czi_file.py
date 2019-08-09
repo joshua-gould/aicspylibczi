5d9723c1facac1413f425210e7f72f037de65234
Jamie Sherman <jamie.sherman@gmail.com>
2019-08-08 17:02:39 -0700

    added test for dimensions of file

--- pylibczi/tests/test_czi_file.py
+++ pylibczi/tests/test_czi_file.py
@@ -43,3 +43,16 @@ def test_read_image_from_istream(data_dir, fname, expected):
         img = czi.read_image()
         print(fname, " => ", img.shape)
     assert img.shape == expected
+
+
+@pytest.mark.parametrize("fname, expects", [
+    ('s_1_t_1_c_1_z_1.czi', {'B': 1, 'C': 1}),  # single dims except for C are dropped (S/T dropped) B is always used
+    ('s_3_t_1_c_3_z_5.czi', {'B': 1, 'S': 3, 'C': 3, 'Z': 5}),
+    ('20190425_S08_001-04-Scene-3-P4-B03.czi', {'B': 1, 'S': 1, 'T': 61, 'C': 2, 'Z': 70})
+])
+def test_image_shape(data_dir, fname, expects):
+    with open(data_dir/fname, 'rb') as fp:
+        czi = CziFile(czi_filename=fp)
+        shape = czi.dims()
+    for key in expects.keys():
+        assert shape[key] == expects[key]
