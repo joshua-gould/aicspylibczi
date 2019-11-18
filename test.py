4a65ab3a86cda9ad80bc3539be6d57c8e6f9d6be
heeler <jamie.sherman@gmail.com>
2019-11-18 11:12:51 -0800

    added reference to docs

--- test.py
+++ test.py
@@ -6,15 +6,12 @@ print("starting Brute Force Test")
 pth = pathlib.Path('./pylibczi/tests/resources/s_1_t_1_c_1_z_1.czi').resolve()
 fp = open(pth, "rb")
 czi = pylibczi.CziFile(fp)
-fp.close()
 print("closing file")
-print(czi.dims())
-
-print("survived continuing to get metadata")
+czi = pylibczi.CziFile(fp)
 
 
-#mta = czi.read_subblock_metadata()
+print("survived")
 
-#print(mta)
+
 
 
