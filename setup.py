7803b46e81cebe91eb9bf7ad1c431d839359714c
heeler <jamie.sherman@gmail.com>
2019-12-18 20:40:58 -0800

    Bump version: 2.3.1 → 2.3.2

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ setup(
     name='aicspylibczi',
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.md
-    version='2.3.1',
+    version='2.3.2',
     author='Jamie Sherman, Paul Watkins',
     author_email='jamies@alleninstitute.org, pwatkins@gmail.com',
     description='A python module and a python extension for Zeiss (CZI/ZISRAW) microscopy files.',
