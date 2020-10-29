be2126f8142835e1aff65c665bce636585d261ca
Jamie Sherman <jamie.sherman@gmail.com>
2020-10-29 11:49:10 -0700

    Update/submodule uri (#52)
    * updated submodule uri to enable automated builds - will revert once libCZI absorbs list of PR's in queue
    * changed action to use custom entrypoint.sh that does a `python setup.py bdist_wheel` instead of `pip wheel` see allen-cell-animated/python-wheels-manylinux-build

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
 import os
+from pathlib import Path
 import platform
 import re
 import subprocess
@@ -86,6 +87,7 @@ class CMakeBuild(build_ext):
             raise RuntimeError("CMake must be installed to build the following extensions: " +
                                ", ".join(e.name for e in self.extensions))
 
+        cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
         if platform.system() == "Windows":
             cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
             if cmake_version < '3.1.0':
@@ -95,6 +97,9 @@ class CMakeBuild(build_ext):
             self.build_extension(ext)
 
     def build_extension(self, ext):
+        path_var = os.environ.get('PATH')
+        path_var = str(Path(sys.executable).parent) + ':' + path_var
+        env = dict(os.environ.copy(), PATH=path_var)
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
@@ -110,13 +115,12 @@ class CMakeBuild(build_ext):
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
             build_args += ['--', '-j2']
 
-        env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
                                                               self.distribution.get_version())
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.', '--target', '_aicspylibczi'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.', '--target', '_aicspylibczi'] + build_args, cwd=self.build_temp, env=env)
 
 setup(
     name='aicspylibczi',
